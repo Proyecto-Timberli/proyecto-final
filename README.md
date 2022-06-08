0854500c8ecede7a9dafd6f6aa76d4b495a823a9
LucianoMocchegiani <lucianomocchegiani@hotmail.com.ar>
2022-06-08 14:58:12 -0300

    configuracion actualizada

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # proyecto-final
+crear base de datos:
+comando en consola
+  create database proyectofinal;
+crear carpeta .env en la api y completar  DB_USER, DB_PASSWORD ,DB_HOST
+
--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import {Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import NavBar from '../src/components/navbar/NavBar.js'
+import LandingPage from '../src/components/landingPage/LandingPage.js'
+import Login from '../src/components/login/Login.js';
+import Register from '../src/components/register/Register.js'
+import Home from '../src/components/home/Home.js'
+import User from '../src/components/user/User.js'
+import Project from '../src/components/project/Project.js'
+import NewProject from '../src/components/newProject/NewProject.js'
+import About from '../src/components/about/About.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <Routes>
+          <Route exact path="/" element = {<LandingPage/>} />
+          <Route exact path="/login" element = {<Login />} />
+          <Route path='/register' element={<Register/>}/>
+          <Route exact path="/home" element = {<Home />} />
+          <Route exact path="/user/:id" element = {<User/>} />
+          <Route exact path="/project/:id" element = {<Project/>} />
+          <Route exact path="/newProject" element = {<NewProject/>} />
+          <Route exact path="/about" element = {<About/>} />
+      </Routes>
     </div>
+      
   );
 }
 
