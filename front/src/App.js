65f219ffd3512177f9113b9704df2f3305fdd229
Andres Gomez Guardamagna <andresgomezguardamagna@gmail.com>
2022-06-09 10:24:28 -0300

    Componente Modal corregido

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import {Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from '../src/components/navbar/NavBar.js'
 import LandingPage from '../src/components/landingPage/LandingPage.js'
@@ -9,22 +9,23 @@ import User from '../src/components/user/User.js'
 import Project from '../src/components/project/Project.js'
 import NewProject from '../src/components/newProject/NewProject.js'
 import About from '../src/components/about/About.js'
+import Modal from './components/modal/modal';
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
       <Routes>
-          <Route exact path="/" element = {<LandingPage/>} />
-          <Route exact path="/login" element = {<Login />} />
-          <Route path='/register' element={<Register/>}/>
-          <Route exact path="/home" element = {<Home />} />
-          <Route exact path="/user/:id" element = {<User/>} />
-          <Route exact path="/project/:id" element = {<Project/>} />
-          <Route exact path="/newProject" element = {<NewProject/>} />
-          <Route exact path="/about" element = {<About/>} />
+        <Route exact path="/" element={<Modal />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/user/:id" element={<User />} />
+        <Route exact path="/project/:id" element={<Project />} />
+        <Route exact path="/newProject" element={<NewProject />} />
+        <Route exact path="/about" element={<About />} />
       </Routes>
     </div>
-      
+
   );
 }
 
--- front/src/components/modal/modal.js
+++ front/src/components/modal/modal.js
@@ -1,14 +1,15 @@
 import React from 'react';
+//import { useDispatch, useSelector } from 'react-redux';
 
-
+import "./modal.css"
 
 const Modal = () => {
     //  const dispatch = useDispatch()
     //    let mensajeState = useSelector((state) => state.mensaje)
-
+    let mensajeState = "Mensaje Temporal"
     let onClickButton = (e) => {
         e.preventDefault();
-        dispatch(crearMensajeState(""))
+        // dispatch(crearMensajeState(""))
     }
     return (
         <div className='modal'>
