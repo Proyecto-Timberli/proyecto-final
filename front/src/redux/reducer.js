6331be44b337fd6829050142a9816f3ec7ef06bb
krosul <deathtrokers@gmail.com>
2022-06-10 12:46:53 -0500

    creacion del store boceto

--- front/src/redux/reducer.js
+++ front/src/redux/reducer.js
@@ -2,10 +2,15 @@
  * Reducer de nuestra aplicación
  */
 
-import { SAMPLE_ACTION } from "./actions/actions";
+import { SAMPLE_ACTION, GET_ALL_PROJECTS, GET_PROJECT_BY_ID, GET_USER_BY_ID, LOGGED, RESET_PROJECT_BY_ID, RESET_USER_BY_ID, SERVER_MESSAGE } from "./actions/actions.js";
 
 const initialState = {
-    test: "test"
+    allProject: [],
+    userById: [],
+    projectById: [],
+    logged: false,
+    server_message: '',
+
 }
 
 export default function reducer(state = initialState, action) {
@@ -15,7 +20,50 @@ export default function reducer(state = initialState, action) {
                 ...state,
                 test: action.payload
             }
-    
+        case GET_ALL_PROJECTS:
+            return {
+                ...state,
+                allProject: action.payload
+            }
+        case GET_PROJECT_BY_ID: {
+            return {
+                ...state,
+                projectById: action.payload
+            }
+        }
+        case GET_USER_BY_ID: {
+            return {
+                ...state,
+                userById: action.payload
+            }
+        }
+        case LOGGED: {
+            return state.logged ? {
+                ...state,
+                logged: false
+            } : {
+                ...state,
+                logged: true
+            }
+        }
+        case RESET_PROJECT_BY_ID: {
+            return {
+                ...state,
+                projectById: action.payload
+            }
+        }
+        case RESET_USER_BY_ID: {
+            return {
+                ...state,
+                userById: action.payload
+            }
+        }
+        case SERVER_MESSAGE: {
+            return {
+                ...state,
+                server_message: action.payload
+            }
+        }
         default:
             return state
     }
