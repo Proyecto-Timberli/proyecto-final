0f4167bd665d3ee1fbabc02c9d2e99b217f80d00
Mica Schaberger <micaschaberger@Air-de-Micaela.fibertel.com.ar>
2022-06-11 19:10:26 -0300

    css register

--- front/src/components/register/Register.js
+++ front/src/components/register/Register.js
@@ -1,43 +1,44 @@
 import React from 'react'
-//import './register.css'
+import './register.css'
 import imgSignUp from './signup-image.png'
+
 function Register() {
   return (
     <div>
         <section class="signup">
-            <div class="container">
+            <div class="container-register">
                 <div class="signup-content">
                     <div class="signup-form">
-                        <h2 class="form-title">Sign up</h2>
+                        <h1 class="form-title">Sign up</h1>
                         <form method="POST" class="register-form" id="register-form">
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="name" id="name" placeholder=" Nombre"/>
+                                <input type="text" className="register-input" placeholder=" Nombre"/>
                             </div>
                             <div class="form-group">
                                 <label for="email"><i class="zmdi zmdi-email"></i></label>
-                                <input type="email" name="email" id="email" placeholder=" Email"/>
+                                <input type="email" className="register-input"  placeholder="Email"/>
                             </div>
                             <div class="form-group">
                                 <label for="pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="pass" id="pass" placeholder="Password"/>
+                                <input type="password" className="register-input"  placeholder="Password"/>
                             </div>
                             <div class="form-group">
                                 <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
-                                <input type="password" name="re_pass" id="re_pass" placeholder="Repite tu password"/>
+                                <input type="password" className="register-input"  placeholder="Repite tu password"/>
                             </div>
                             <div class="form-group">
                                 <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
                                 <label for="agree-term" class="label-agree-term"><span><span></span></span>Acepto todos los   <a href="#" class="term-service">términos y condiciones</a></label>
                             </div>
-                            <div class="form-group form-button">
+                            <div class="form-button">
                                 <input type="submit" name="signup" id="signup" class="form-submit" value="Registrar"/>
                             </div>
                         </form>
                     </div>
                     <div class="signup-image">
-                        <figure><img src={imgSignUp} alt="sing up image"/></figure>
-                        <a href="/login" class="signup-image-link">Ya soy miembro, Quiero Logearme</a>
+                        <figure><img src={imgSignUp} alt="sing up"/></figure>
+                        <a href="/login" class="signup-image-link">Ya soy miembro, quiero logearme</a>
                     </div>
                 </div>
             </div>
