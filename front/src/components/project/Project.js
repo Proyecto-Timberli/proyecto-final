e9734d9a3c91983146c58c78c290a02dec2d716d
Andres Gomez Guardamagna <andresgomezguardamagna@gmail.com>
2022-06-12 18:51:11 -0300

    unique key <img> detail project

--- front/src/components/project/Project.js
+++ front/src/components/project/Project.js
@@ -1,4 +1,4 @@
-import React, { useEffect , useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import imagen from './signup-image.png'
 import { Link, useParams } from 'react-router-dom'
 import './project.css'
@@ -32,10 +32,10 @@ function Project() {
             ...cardsInPag,
             ...paginado.paginar(selectPag, selectFilter)
         })
-    } 
+    }
     useEffect(() => {
-        if (Object.keys(project).length && project.imagen.length){
-            accionarPaginado(1)       
+        if (Object.keys(project).length && project.imagen.length) {
+            accionarPaginado(1)
         }
     }, [project])
     ////////////////////////////////////////////////////////////////////////////////////////
@@ -58,37 +58,37 @@ function Project() {
     // user: { id: 1, name: 'Luciano', userName: 'luciano', mail: 'luciano@mail.com', password: 'password', … }
     // userId: 
     if (!Object.keys(project).length) return <Cargando />
-   
+
     return (
-       
+
         <React.Fragment>
             <div className='detail-container'>
                 <div className='project-title-container'><h2 className='project-title'>{project.name}</h2></div>
                 <div className='Contenedor-detalles'>
-                <div className='cont-info'>              
+                    <div className='cont-info'>
                         <div>
                             <h3>Puntuacion:</h3>
-                            <div className='info-detalle' >{(project.score[0]|project.score[1]|project.score[2])}</div>
+                            <div className='info-detalle' >{(project.score[0] | project.score[1] | project.score[2])}</div>
                         </div>
                         <div >
                             <h3>Usuario:</h3>
                             <div className='info-detalle' >{project.user.name}</div>
-                        </div>                          
+                        </div>
                     </div>
-                
+
                     <div className='cont-img-detalle'>
-                        {cardsInPag.renderCards.map(image => (!!image)&&
-                           <img className='card-img-detalle' src={image}></img> )}
+                        {cardsInPag.renderCards.map(image => (!!image) &&
+                            <img key={image} className='card-img-detalle' src={image}></img>)}
                         <div className="project-paginado-button-container">
                             {paginado.buttons().map(button =>
-                            <div key={button}>
-                              {cardsInPag.pag !== button && <button className="project-paginado-button" onClick={() => accionarPaginado(button)}></button>}
-                              {cardsInPag.pag === button && <button className="project-paginado-button-select" onClick={() => accionarPaginado(button)}></button>}
-                            </div>
+                                <div key={button}>
+                                    {cardsInPag.pag !== button && <button className="project-paginado-button" onClick={() => accionarPaginado(button)}></button>}
+                                    {cardsInPag.pag === button && <button className="project-paginado-button-select" onClick={() => accionarPaginado(button)}></button>}
+                                </div>
                             )}
-                        </div> 
+                        </div>
                     </div>
-                    <div className='cont-info'>              
+                    <div className='cont-info'>
                         <div >
                             <h3>Deploy:</h3>
                             {project.deploying === "none" ? <div className='info-detalle-link'>Sin Deploy</div> : <div className='info-detalle-link' ><a href={project.deploying}> Link Deploy</a></div>}
@@ -98,7 +98,7 @@ function Project() {
                         <div>
                             <h3>GitHub:</h3>
                             {project.repository === "none" ? <div className='info-detalle-link'>Sin GitHub</div> : <div className='info-detalle-link' ><a href={project.repository}> Link GitHub</a></div>}
-                        </div>                             
+                        </div>
 
                     </div>
                 </div>
@@ -107,11 +107,11 @@ function Project() {
                     <div className='desc-detalle'>
                         <h3 >Descripcion:</h3>
                         <div>
-                          <p className ="text">{project.description}</p>
+                            <p className="text">{project.description}</p>
                         </div>
                         <h3>Tecnologias:</h3>
                         <div>
-                          <p className ="text">{project.tecnology}</p> 
+                            <p className="text">{project.tecnology}</p>
                         </div>
                     </div>
                 </div>
