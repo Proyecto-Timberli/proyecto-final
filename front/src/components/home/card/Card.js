6ca34ee8ba1d36d9b30c0176d171065657134a87
Mica Schaberger <micaschaberger@Air-de-Micaela.fibertel.com.ar>
2022-06-16 12:03:47 -0300

    Score agregado y link al usuario

--- front/src/components/home/card/Card.js
+++ front/src/components/home/card/Card.js
@@ -4,14 +4,16 @@ import defaultImg from './signup-image.png'
 import linkedin from '../../../images/linkedin.png';
 import github from '../../../images/github.png';
 import { Link } from 'react-router-dom';
-function Card({ id, name, description, user, imagen }) {
-
-
+function Card({ id, name, description, user, imagen, userId, score }) {
+    const sum = score.reduce((primerScore, siguienteScore) => primerScore + siguienteScore, 0);
+    const promedio = sum/score.length
+    
     return (
         <div key={id} >
             <div className="card-home">
-
-                <div className="card-user">{user.toUpperCase()}</div>
+                <Link to={"/user/" + userId} className="">
+                    <div className="card-user">{user.toUpperCase()}</div>
+                </Link>
                 <div className="card-img" >
                     {
                         imagen.length > 0 ? 
@@ -28,6 +30,7 @@ function Card({ id, name, description, user, imagen }) {
                 <div className="card-info-home">
                     <p className="text-title-card">{name}</p>
                     <p className="text-body-card">{description}</p>
+                    <p className='text-score-card'>Total score: {promedio}</p>
                     <Link to={"/project/" + id} className="card-button-home">Ver Proyecto</Link>
                 </div>
             </div>
