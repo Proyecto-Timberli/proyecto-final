1692a05aba0111e7a4e6e42adb15f5c7751ca7fe
LucianoMocchegiani <lucianomocchegiani@hotmail.com.ar>
2022-06-19 19:27:24 -0300

    se mejoro el controlado de el formulario

--- front/src/components/register/validation.js
+++ front/src/components/register/validation.js
@@ -13,37 +13,35 @@ export default function validation(formData) {
     
     let errors = {}
 
+    
     if (!formData.name) {
-        errors.name = "Debes ingresar tu nombre"
-    }
+        errors.name = '*Debes ingresar tu nombre'
+    }else if (formData.name.length > 20){
+        console.log("entra sssssssssssssssssssss")
+        errors.name = '*caracteres excedidos'
+    }else if (!/^[a-z ,.'-]+$/i.test(formData.name)){
+        errors.name = '*No puede contener simbolos'
+    } 
 
     if (!formData.email) {
-        errors.email = "Debes ingresar tu correo"
-    }
-
-    if (!mailRegex.test(formData.email)) {
-        errors.email = "Debes ingresar un correo válido"
+        errors.email = "*Debes ingresar tu correo"
+    }else if (!mailRegex.test(formData.email)) {
+        errors.email = "*El formato no es valido"
     }
 
     if (!formData.password) {
-        errors.password = "Debes ingresar una contraseña"
-    }
-
-    if (!passwordRegex.test(formData.password)) {
-        console.log()
-        errors.password = "Tu contraseña debe tener almenos: 8 caracteres, una mayúscula, una minúscula y un número"
-    }
-
-    if (formData.password !== formData.repeat_password) {
-        errors.repeat_password = "Las contraseñas no coinciden"
+        errors.password = "*Debes ingresar una contraseña"
+    }else if (!passwordRegex.test(formData.password)) {
+        errors.password = "*Debe contener 8 caracteres, una mayúscula, una minúscula y un número"
     }
 
     if (!formData.repeat_password) {
-        errors.repeat_password = "No has repetido tu contraseña"
+        errors.repeat_password = "*Debes repetir tu contraseña"
+    }else if (formData.password !== formData.repeat_password) {
+        errors.repeat_password = "*Las contraseñas no coinciden"
     }
-
     if (!formData.tos) {
-        errors.tos = "No has aceptado nuestros términos y condiciones"
+        errors.tos = "*No has aceptado nuestros términos y condiciones"
     }
 
     return errors
