bcc91b41431dba659d45b44e454b55d384333618
Sapo-san <cristobal.herreros@gmail.com>
2022-06-19 00:24:26 -0400

    esqueleto de userSettings implementado, sin funcionalidadaun

--- front/src/components/user/displayUserSettings/displayUserSettings.js
+++ front/src/components/user/displayUserSettings/displayUserSettings.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import './displayUserSettings.css'
 
 const DisplayUserSettings = ({ userData }) => {
   return (
-    <div>DisplayUserSettings</div>
+    <div className='userSettingsContainer'>
+      <h3>Edita tu información aquí:</h3>
+
+      <div className='userSettings-firstGroup'>
+        <div className='userSettings-fg-inputContainer'>
+          <p>Edita tu nombre:</p>
+          <input type="text" className='userSettings-textInput' ></input>
+        </div>
+        <div className='userSettings-fg-inputContainer'>
+          <p>Edita tu stack:</p>
+          <input type="text" className='userSettings-textInput' ></input>
+        </div>
+        <div className='userSettings-fg-inputContainer'>
+          <p>Enlace a LinkedIn</p>
+          <input type="text" className='userSettings-textInput' ></input>
+        </div>
+        <div className='userSettings-fg-inputContainer'>
+          <p>Enlace a Github</p>
+          <input type="text" className='userSettings-textInput' ></input>
+        </div>
+      </div>
+      <div className='userSettings-secondGroup'>
+        <div className='userSettings-sg-inputContainer'>
+          <p>Sube una imagen para tu perfil:</p>
+          <input type="text" className='userSettings-textInput' ></input>
+        </div>
+      </div>
+      <div className='userSettings-secondGroup'>
+        <div className='userSettings-sg-inputContainer'>
+          <p>Edita tu descripción (corta):</p>
+          <input type="text" className='userSettings-textInput' ></input>
+        </div>
+        <div className='userSettings-sg-inputContainer'>
+          <p>Edita tu descripción (Sección "Sobre mi"):</p>
+          <textarea className='userSettings-textArea' ></textarea>
+        </div>
+      </div>
+    </div>
   )
 }
 
