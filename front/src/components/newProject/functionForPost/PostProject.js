8fc27c2391bd8e6867a39e6ced4418c370ed6297
Andres Gomez Guardamagna <andresgomezguardamagna@gmail.com>
2022-06-20 15:57:06 -0300

    correccion

--- front/src/components/newProject/functionForPost/PostProject.js
+++ front/src/components/newProject/functionForPost/PostProject.js
@@ -1,12 +1,12 @@
 import axios from "axios"
-const {REACT_APP_CLOUD_NAME,REACT_APP_UPLOAD_PRESET, REACT_APP_API}=process.env
+const { REACT_APP_CLOUD_NAME, REACT_APP_UPLOAD_PRESET, REACT_APP_API } = process.env
 
 const postProject = async ({ deploying, imagen, name, tecnology, description, repository, score, userid }) => {
     let upload_preset = REACT_APP_UPLOAD_PRESET;
     const uploadUrl = imagen.map(async photo => {
         const formData = new FormData()
         formData.append("file", photo)
-        formData.append("upload_preset",upload_preset)
+        formData.append("upload_preset", upload_preset)
         try {
             const res = await axios.post(`https://api.cloudinary.com/v1_1/${REACT_APP_CLOUD_NAME}/image/upload`, formData)
             return res.data.secure_url
@@ -19,10 +19,10 @@ const postProject = async ({ deploying, imagen, name, tecnology, description, re
 
     await axios.all(uploadUrl)
         .then(res => {
-           
+
             const project = { deploying, imagen: res, name, tecnology, description, repository, score, userid }
-          
-            axios.post(REACT_APP_API+"api/project", project)
+
+            axios.post(REACT_APP_API + "/api/project", project)
                 .then(response => response.data)
                 .catch(error => console.error(error))
         }).catch(err => { console.error(err) })
