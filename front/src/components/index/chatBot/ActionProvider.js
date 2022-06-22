75052840879a675acfec9a04e9f657204089c49b
Mica Schaberger <micaela.schaberger@gmail.com>
2022-06-22 12:05:33 -0300

    chatbot creado, sin ni mensajes

---
+++ front/src/components/index/chatBot/ActionProvider.js
@@ -0,0 +1,20 @@
+import 'react-chatbot-kit/build/main.css'
+
+class ActionProvider {
+    constructor(createChatbotMessage, setStateFunc, createClientMessage) {
+      this.createChatbotMessage = createChatbotMessage;
+      this.setState = setStateFunc;
+      this.createClientMessage = createClientMessage;
+    }
+  
+    handleHello() {
+      const message = this.createChatBotmessage('Hello. Nice to meet you.');
+  
+      this.setState((prev) => ({
+        ...prev,
+        messages: [...prev.messages, message],
+      }));
+    }
+}
+  
+  export default ActionProvider;
