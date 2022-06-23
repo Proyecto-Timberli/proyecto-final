144671bafc224d6121267a7d0cf0d4df5fe5a61d
Andres Gomez Guardamagna <andresgomezguardamagna@gmail.com>
2022-06-23 16:54:37 -0300

    Inicio pseudo funcional

--- front/src/components/index/inicio/inicio.js
+++ front/src/components/index/inicio/inicio.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from "react-redux";
+import { getAllProjects } from '../../../redux/actions/actionCreators'
+import { scroll } from "../../../functions";
+import Card from '../../componentesGenerales/card/Card';
+import "./inicio.css"
 
 const Inicio = () => {
+    scroll()
+    let dispatch = useDispatch()
+    let allProjects = useSelector((state) => state.allProject)
+    useEffect(() => {
+        dispatch(getAllProjects());
+    }, [])
+
     return (
-        <div>
-            Hola soy el inicio!
+        <div className='cont-general-inicio'>
+
+            {allProjects.map(e => (!!e) && <Card
+                description={e.shortDescription}
+                key={e.id}
+                name={e.name}
+                id={e.id}
+                user={e.user.name}
+                imagen={e.imagen}
+                userId={e.userId}
+                scoreFunctionality={e.scoreFunctionality}
+                scoreOriginality={e.scoreOriginality}
+                scoreStyle={e.scoreStyle}
+
+                score={e.scoreAverage}
+            />)}
         </div>
     );
 }
