5859b25a2785e4b71f4a78a1550e3978518ab2bd
Andres Gomez Guardamagna <andresgomezguardamagna@gmail.com>
2022-06-23 15:53:35 -0300

    funcion para fechas estilizadas

--- front/src/functions/index.js
+++ front/src/functions/index.js
@@ -27,3 +27,15 @@ export function ordenar(array, tipoOrden) {
         })
     }
 }
+
+const dateOptions = {
+    timeZone: 'UTC',
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+};
+export const formatDate = date => {
+    const newDate = new Date(date);
+    return newDate.toLocaleDateString('es-ES', dateOptions);
+};
