86a1d92174b146a57d9064e1a996f86f56748cf7
Mica Schaberger <micaela.schaberger@gmail.com>
2022-06-24 12:09:05 -0300

    boton y contribuciones

--- front/src/components/panelAdmin/contributions/contributions.js
+++ front/src/components/panelAdmin/contributions/contributions.js
@@ -14,25 +14,33 @@ const Contributions = () => {
 
     return (
         <div className='cont-cont-general'>
-
-
             <h1>
-                Listado de Contribuidores
+                CONTRIBUCIONES
             </h1>
             <Link className='volver-admin' to='/admin'> Volver al Panel</Link>
-            <div className='contribuciones-cont'>
-
-                {contribuciones.map(e =>
-                    <div className='contribuciones-card' key={e.id}>
-                        <label>Contribucion #{e.id} </label>
-                        <div>Contribuidor: {e.name} </div>
-                        <div>Cantidad: {e.amount} U$D</div>
-
-                    </div>
-                )}
+            
+            <div className='contribuciones-container'>
+                <table>
+                    <tr className='table-header'>
+                        <th className='columna-header'>Numero de contribucion</th>
+                        <th className='columna-header'>Id usuario</th>
+                        <th className='columna-header'>Nombre de contribuidor</th>
+                        <th className='columna-header'>Cantidad contribuida</th>
+                    </tr>
+                    {contribuciones.map(e =>
+                        <tr>
+                            <td className='columna-contribucion'> # {e.id} </td>
+                            {e.userId ? <td className='columna-contribucion'> {e.userId}</td> : <td className='columna-contribucion'> No tiene </td>}
+                            <td className='columna-contribucion'> {e.name} </td>
+                            <td className='columna-contribucion'> {e.amount} U$D </td>
+                        </tr>
+                    )}
+                </table>
             </div>
-        </div >
+
+        </div>
     );
 }
+
 
 export default Contributions;
